de88934d01b98f4e422a283cbe1e74cc425ac61f
FedeChicca <darkrose094@gmail.com>
2022-04-28 09:50:54 +0200

    Recensioni

---
+++ recensioni.js
@@ -0,0 +1,15 @@
+const starWrapper = document.querySelectorAll('.star');
+const stars = document.querySelectorAll('.stars a');
+
+stars.forEach((star, clickedIdx)=> {
+    star.addEventListener("click", () => {
+        starWrapper.classList.add("disabled");
+        stars.forEach((otherStar, otherIdx)=> {
+            if (otherIdx <= clickedIdx) {
+                otherStar.classList.add("active");
+            }
+        
+        });
+        console.log('star of index ${clickedIdx + 1} was clicked')
+    });
+});
